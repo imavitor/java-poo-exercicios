5104613d4bb1c178cbbd286e11b3c0cf7d688a15
Vitor Imamura <vitor.imamura@protonmail.com>
2026-03-04 17:13:11 -0300

    Enviando arquivos

--- Exercicio2/src/Application/Programa.java
+++ Exercicio2/src/Application/Programa.java
@@ -1,7 +1,31 @@
 package Application;
+import java.util.Scanner;
+import java.util.Locale;
+import Entities.Employee;
 
 public class Programa {
 
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
+        Employee empregado = new Employee();
 
+        System.out.println("Digite o nome do empregado: ");
+        empregado.name = sc.nextLine();
+        System.out.println("Digite o salario do empregado: ");
+        empregado.GrossSalary = sc.nextDouble();
+        System.out.println("Digite o imposto: ");
+        empregado.Tax = sc.nextDouble();
+
+        System.out.println(empregado);
+
+        System.out.println("Qual porcentagem para aumentar o salário? ");
+        double percent = sc.nextDouble();
+        empregado.IncreaseSalary(percent);
+        System.out.println("Dados atualizados: ");
+        System.out.println(empregado);
+
+        sc.close();
+    }
 }
--- Exercicio2/src/Entities/Employee.java
+++ Exercicio2/src/Entities/Employee.java
@@ -3,7 +3,24 @@ package Entities;
 public class Employee {
 
     public String name;
-    public int GrossSalary;
+    public double GrossSalary;
     public double Tax;
 
+    public double NetSalary(){
+        return GrossSalary - Tax;
+    }
+
+    public void IncreaseSalary(double percentage){
+
+        GrossSalary += GrossSalary * (percentage / 100.0);
+
+    }
+
+    @Override
+    public String toString() {
+        return "Nome do empregado: "
+                + name
+                + "\n"
+                + String.format("Salário Líquido: %.2f\n", NetSalary());
+    }
 }
