57fc5c68ffd500699da6320955844d5df465da97
Vitor Imamura <vitor.imamura@protonmail.com>
2026-03-04 17:12:52 -0300

    Enviando arquivos

---
+++ Exercicio2/src/Application/Programa.java
@@ -0,0 +1,7 @@
+package Application;
+
+public class Programa {
+
+
+
+}
---
+++ Exercicio2/src/Entities/Employee.java
@@ -0,0 +1,9 @@
+package Entities;
+
+public class Employee {
+
+    public String name;
+    public int GrossSalary;
+    public double Tax;
+
+}
